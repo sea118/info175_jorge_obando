06c0cc8422da197f8f0245d79a23b77c966a5165
jorge obando <jorge obando>
2016-03-17 12:37:30 -0400

    primer ejercicio

---
+++ mayor.py
@@ -0,0 +1,16 @@
+def mayor(x, y):
+    if x < y:
+      print ("El mayor es ",y)
+      print ("El menor es ",y)
+    elif  x > y:
+      print ("El mayor es ",x)
+      print ("El menor es ",y)
+    else:
+
+        print ("Son iguales ")
+    
+if __name__== "__main__":
+    a = int(input("ingrese un numero: "))
+    b = int(input("ingrese un numero: "))
+
+mayor(a, b)
