e620ee066a0cc934eaf64d3f7da5f1351a8ed096
sea118 <sebita_118@hotmail.com>
2016-03-17 21:21:08 -0300

    Update mayor.py

--- mayor.py
+++ mayor.py
@@ -1,7 +1,7 @@
 def mayor(x, y):
     if x < y:
       print ("El mayor es ",y)
-      print ("El menor es ",y)
+      print ("El menor es ",x)
     elif  x > y:
       print ("El mayor es ",x)
       print ("El menor es ",y)
